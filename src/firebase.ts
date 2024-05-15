c89d7ab1661055e3321af05e3a7cf2ec8b6d264e
Miko Huang <37956537+chi628@users.noreply.github.com>
2024-05-16 00:06:18 +0800

    log env

--- src/firebase.ts
+++ src/firebase.ts
@@ -14,6 +14,7 @@ const firebaseConfig = {
 }
 
 export async function readData() {
+  console.log('env', import.meta.env.VITE_APP_ID, import.meta.env)
   const db = getDatabase()
   const _ref = ref(db)
 
